50f7a6bf21c74785324d5a48b104c183896964e2
Praveen Kumar <praveen.kumar@comprotechnologies.com>
2024-12-24 19:10:57 +0530

    name change as per feedback

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
             type: "doc",
             docId: "intro",
             position: "left",
-            label: "Documents",
+            label: "Docs",
           },
           {
             label: "APIs",
